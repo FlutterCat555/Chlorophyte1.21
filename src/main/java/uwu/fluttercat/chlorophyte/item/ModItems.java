ca5cfc22fe506d15941fcd90e1e479f9fc71d95c
FlutterCat555 <griffinyoshi@gmail.com>
2024-10-20 16:44:21 -0400

    test

--- src/main/java/uwu/fluttercat/chlorophyte/item/ModItems.java
+++ src/main/java/uwu/fluttercat/chlorophyte/item/ModItems.java
@@ -3,6 +3,7 @@ package uwu.fluttercat.chlorophyte.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.SwordItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -11,8 +12,13 @@ import uwu.fluttercat.chlorophyte.Chlorophyte;
 public class ModItems {
     public static final Item DEBUGGER = registerItem("debugger", new Item(new Item.Settings()));
     public static final Item CHLOROPHYTE = registerItem("chlorophyte", new Item(new Item.Settings()));
-    public static final Item CHLOROPHYTE_SWORD = registerItem("chlorophyte_sword", new Item(new Item.Settings()));
-    public static final Item CHLOROPHYTE_SCYTHE = registerItem("chlorophyte_scythe", new Item(new Item.Settings()));
+    public static final Item CHLOROPHYTE_SWORD = registerItem("chlorophyte_sword", new
+            SwordItem(ModToolMaterials.CHLOROPHYTE,new Item.Settings()
+            .attributeModifiers(SwordItem.createAttributeModifiers(ModToolMaterials.CHLOROPHYTE,2,-1f))));
+    public static final Item CHLOROPHYTE_SCYTHE = registerItem("chlorophyte_scythe", new
+            SwordItem(ModToolMaterials.CHLOROPHYTE,new Item.Settings()
+            .attributeModifiers(SwordItem.createAttributeModifiers(ModToolMaterials.CHLOROPHYTE,4,-2.6f))));
+    public static final Item CHLOROPHYTE_ACTUATOR = registerItem("chlorophyte_actuator", new Item(new Item.Settings()));
 
 
     private static Item registerItem(String name, Item item) {
